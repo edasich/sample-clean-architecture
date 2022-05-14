246962f921cd2cb2213e1e1dfbb8a870fea29cec
47even <47even@gmail.com>
2022-05-14 18:01:14 +0430

    feat: add default param for start location use case.

--- location-service/src/main/java/com/github/edasich/location/service/StartLocationCollector.kt
+++ location-service/src/main/java/com/github/edasich/location/service/StartLocationCollector.kt
@@ -3,5 +3,7 @@ package com.github.edasich.location.service
 import com.github.edasich.location.domain.LocationConfig
 
 interface StartLocationCollector {
-    operator fun invoke(locationConfig: LocationConfig)
+    operator fun invoke(
+        locationConfig: LocationConfig = LocationConfig.Builder().build()
+    )
 }
