b72eb856f2b8df6ec8a6ab046b39d51068da2b71
47even <47even@gmail.com>
2022-05-14 18:07:08 +0430

    feat: apply the model into PlaceGeocode.

--- place-finder-domain/src/main/java/com/github/edasich/place/finder/domain/PlaceGeocode.kt
+++ place-finder-domain/src/main/java/com/github/edasich/place/finder/domain/PlaceGeocode.kt
@@ -1,9 +1,6 @@
 package com.github.edasich.place.finder.domain
 
-import com.github.edasich.location.domain.Latitude
-import com.github.edasich.location.domain.Longitude
-
 data class PlaceGeocode(
-    val latitude : Latitude,
-    val longitude: Longitude
+    val latitude: PlaceLatitude,
+    val longitude: PlaceLongitude
 )
